a27b7ca55af36aad22ae9a8af95f600552222def
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-26 16:01:30 -0230

    html content now all written in JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css'
-import compile from './load.js'
+import compile from './initial-load.js'
 
 compile();
 
--- src/initial-load.js
+++ src/initial-load.js
@@ -1,25 +1,31 @@
 export default function compile() {
     const contentId = document.getElementById('content');
-    console.log(contentId);
 
     const headEl = document.createElement('div');
     headEl.className = "head-cont";
     
     const title = document.createElement('h1');
     title.textContent = "Jerry's Pizza";
+
     headEl.appendChild(title);
     contentId.appendChild(headEl);
-}
 
-//div class="head-cont">
-    //<h1>Jerry's Pizza</h1>
-//</div>
-//<div class="main">
-    //<div class="menu">
-        //<p>aglihraui afabfae faewbwffefb ew bewkewabewfbfbewfb bbe abuffbfba bfbb abeb akfbbaw bfkfbifbwabew b bfewwejfbwef bfaw bwk</p>
-    //</div>
-//</div>
-//<div class="foot-cont">
-    //<p>Photo by <a href="https://unsplash.com/@pablopacheco_videografo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-        //Pablo Pacheco</a> on <a href="https://unsplash.com/images/food/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
-//</div>
+    const footEl = document.createElement('div');
+    footEl.className = "foot-cont";
+
+    const credit = document.createElement('p');
+    credit.innerHTML = 'Photo by <a href="https:unsplash.com/@pablopacheco_videografo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pablo Pacheco</a> on <a href="https://unsplash.com/images/food/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>';
+    
+    footEl.appendChild(credit);
+    contentId.appendChild(footEl);
+
+    const mainEl = document.createElement('div');
+    mainEl.className = 'main';
+    const menuEl = document.createElement('div');
+    menuEl.className = 'menu';
+    const menuPraise = document.createElement('p');
+    menuPraise.textContent = "aglihraui afabfae faewbwffefb ew bewkewabewfbfbewfb bbe abuffbfba bfbb abeb akfbbaw bfkfbifbwabew b bfewwejfbwef bfaw bwk";
+    menuEl.appendChild(menuPraise);
+    mainEl.appendChild(menuEl);
+    contentId.appendChild(mainEl);
+}
