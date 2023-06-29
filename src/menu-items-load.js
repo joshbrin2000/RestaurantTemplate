909861f4b6314f1fd126aec92e2c09846a5da241
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-29 18:56:39 -0230

    fixed layout issues and added more menu options

--- src/menu-items-load.js
+++ src/menu-items-load.js
@@ -8,7 +8,9 @@ export default function menuSection(el) {
         let newDiv = document.createElement('div');
         newDiv.className = 'menu-item sect' + i;
         descriptDiv(newDiv, i);
-        pictureLoad(newDiv, i);
+        if (i < 7){
+            pictureLoad(newDiv, i);
+        }
         menuItemGrid.appendChild(newDiv);
     }
 
@@ -20,22 +22,28 @@ function descriptDiv(el, i) {
     let descDiv = document.createElement('div');
     switch (i) {
         case 1:
-            descDiv.innerHTML = '<p>Pepperoni Pizza - hand-made with care</p><br /><p>Price: $15.99</p>';
+            descDiv.innerHTML = '<p>Pepperoni Pizza - hand-made with care</p><p>Price: $15.99</p>';
             break;
         case 2:
-            descDiv.innerHTML = '<p>Cheese Pizza - using 2-month-old marble cheese found at the dumpster at Walmart</p><br /><p>Price: $11.99</p>';
+            descDiv.innerHTML = '<p>Cheese Pizza - using 2-month-old marble cheese found at the dumpster at Walmart</p><p>Price: $11.99</p>';
             break;
         case 3:
-            descDiv.innerHTML = '<p>Cheeseburger - popular with our kid customers</p><br /><p>Price: $9.99</p>';
+            descDiv.innerHTML = '<p>Cheeseburger - popular with our kid customers</p><p>Price: $9.99</p>';
             break;
         case 4:
-            descDiv.innerHTML = '<p>Chicken Tenders <sub>not to be confused with our previous menu item, Deep-Fried Chicken Beaks, of which we are not allowed to sell anymore due to so-called "Health Concerns"</sup></p><br /><p>Price: $8.49</p>';
+            descDiv.innerHTML = '<p>Chicken Tenders <sub>not to be confused with our previous menu item, Deep-Fried Chicken Beaks, of which we are not allowed to sell anymore due to so-called "Health Concerns"</sup></p><p>Price: $8.49</p>';
             break;
         case 5:
-            descDiv.innerHTML = '<p>Mozzarella Sticks</p><br /><p>Price: $4.99</p>';
+            descDiv.innerHTML = '<p>Mozzarella Sticks</p><p>Price: $4.99</p>';
             break;
         case 6:
-            descDiv.innerHTML = '<p>Caesar Salad</p><br /><p>Price: $7.99</p>';
+            descDiv.innerHTML = '<p>Caesar Salad</p><p>Price: $7.99</p>';
+            break;
+        case 7:
+            descDiv.innerHTML = '<h3>Sides</h3><br /><p>Fries: $2.99</p><p>Garden Salad: $3.49</p><p>Onion Rings (old): $2.99</p><p>Onion Rings (fresh): $4.49</p>';
+            break;
+        case 8:
+
             break;
         default:
             break;
